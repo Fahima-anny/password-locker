b5d53cf87509275b3c0453ebacc0f75a781d7af0
Fahima-anny <fahima.info03@gmail.com>
2024-03-11 22:30:18 +0600

    second commit

--- app2.js
+++ app2.js
@@ -18,67 +18,105 @@ let generatePin = document.getElementById("generatePin") ;
 generatePin.addEventListener("click" , function(){
     let pinInp = document.getElementById("pinInp") ;
     pinInp.value = checker() ;
+    document.getElementById("typeNumberField").value = ""
+    document.getElementById("pinMatched").style.display = "none"
+document.getElementById("pinUnmatched").style.display = "none"
 })
 
-document.getElementById("btn1").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 1
+document.getElementById("calculator").addEventListener("click",function(event){
+  let number = event.target.innerText
+  let typeNumberField = document.getElementById("typeNumberField")
+  let previousTypeNumber = typeNumberField.value
+  console.log(previousTypeNumber)
+
+  if(isNaN(number)){
+    if(number === "C"){
+        typeNumberField.value = ""
+    }
+    else if(number === "<"){
+let digit = previousTypeNumber.split("");
+digit.pop();
+let remaining = digit.join('');
+typeNumberField.value =remaining;
+console.log(remaining);
+    }
+  }
+  else{
+    let newTypeNumber = previousTypeNumber + number
+    typeNumberField.value = newTypeNumber
+  }
 })
 
-document.getElementById("btn2").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 2
-})
-document.getElementById("btn3").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 3
-})
-document.getElementById("btn4").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 4
-})
-document.getElementById("btn5").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 5
-})
-document.getElementById("btn6").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 6
-})
-document.getElementById("btn7").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 7
-})
-document.getElementById("btn8").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 8
-})
-document.getElementById("btn9").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 9
-})
-// document.getElementById("btn>").addEventListener("click",function(){
+
+// document.getElementById("btn1").addEventListener("click",function(){
 //     let applyPin = document.getElementById("applyPin")
-//     applyPin.value = applyPin.value 
+//     applyPin.value = applyPin.value + 1
 // })
-document.getElementById("btn0").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = applyPin.value + 0
-})
-document.getElementById("btnC").addEventListener("click",function(){
-    let applyPin = document.getElementById("applyPin")
-    applyPin.value = ""
-})
+
+// document.getElementById("btn2").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 2
+// })
+// document.getElementById("btn3").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 3
+// })
+// document.getElementById("btn4").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 4
+// })
+// document.getElementById("btn5").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 5
+// })
+// document.getElementById("btn6").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 6
+// })
+// document.getElementById("btn7").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 7
+// })
+// document.getElementById("btn8").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 8
+// })
+// document.getElementById("btn9").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 9
+// })
+// // document.getElementById("btn>").addEventListener("click",function(){
+// //     let applyPin = document.getElementById("applyPin")
+// //     applyPin.value = applyPin.value 
+// // })
+// document.getElementById("btn0").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = applyPin.value + 0
+// })
+// document.getElementById("btnC").addEventListener("click",function(){
+//     let applyPin = document.getElementById("applyPin")
+//     applyPin.value = ""
+// })
+document.getElementById("pinMatched").style.display = "none"
+document.getElementById("pinUnmatched").style.display = "none"
 
 let submitBtn =document.getElementById("submitBtn") ;
 submitBtn.addEventListener("click" , function(){
-if(applyPin.value == pinInp.value ){
-    applyPin.value = "Congratulation!!!" ;
-    document.getElementById("pinMatched").classList.remove("d-none")
+if(typeNumberField.value == pinInp.value ){
+    typeNumberField.value = "Congratulation!!!" ;
+    document.getElementById("pinMatched").style.display = "block"
+    document.getElementById("pinUnmatched").style.display = "none"
+    document.getElementById("retryTxt").style.display = "none"
 }
 else{
-    applyPin.value = "Sorry!!!" ;
-    document.getElementById("pinUnmatched").classList.remove("d-none")
+    typeNumberField.value = "Sorry!!!" ;
+    document.getElementById("pinUnmatched").style.display = "block"
+    document.getElementById("pinMatched").style.display = "none"
+    let retryNumber = document.getElementById("retryNumber")
+    let retryNumberTxt = retryNumber.innerText
+    let retryNumberInt = parseInt(retryNumberTxt)
+    retryNumber.innerText = retryNumberInt - 1
+   
 }
 })
 
